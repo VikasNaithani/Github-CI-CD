95c2314840f05ad34fbaae3103260e846c418806
Vikas Naithani <132071075+VikasNaithani@users.noreply.github.com>
2024-12-09 13:40:29 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -11,9 +11,9 @@ const config = {
           debug: true,
           testops: {
             api: {
-              token: '7f2067fbe9739d010cb390a37ddef7187862907b090de9035b045e4ea0f294ef',
+              token: '',
             },
-            project: 'EW',
+            project: '',
             uploadAttachments: true,
             run: {
               complete: true,
